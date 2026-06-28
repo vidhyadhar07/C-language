7d642a620b4a3b1b1071ea8331582613d4bfe5e9
vidhyadhar chittimalla <chittimallavidhyadhar2007@gmail.com>
2026-06-29 00:16:06 +0530

    Basic arithmetic operations in code2.c

---
+++ code2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+/*int grade=9;
+long income=150000;
+float temperature=36.8;
+double pi=3.142857;
+char var[]= "vidhyadhar";
+  printf("Grade=%d\n",grade);
+  printf("Income=%ld\n",income);
+  printf("Temperature=%f\n",temperature);
+  printf("Pi=%lf\n",pi);
+  printf("Name=%s\n",var);*/
+
+// printf("2+3=%d\n",2+3);
+// int a=5,b=10;
+  //printf("a=%d b=%d\n",a,b);
+  float c=30;
+  float d=7;
+  printf("quotient=%f\n",c/d);
+  /*int e=70;
+  int f=4;
+  printf("quoitent=%f",e/f);*/
+  int g=10;
+  int h=3;  
+  int sum= g+h;
+  int sub=g-h;
+  printf("sum=%d\n",sum);
+  printf("sub=%d",sub);
+return 0;
+}
